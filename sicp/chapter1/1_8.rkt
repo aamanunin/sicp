03fb0861432b1379bcb1b3b7bf7754a76041b695
Aleksandr Maniunin <aamanunin@gmail.com>
2020-01-10 00:45:10 +0700

    complete exercise 1_8

---
+++ sicp/chapter1/1_8.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (cube x) (* x x x))
+
+(define (good-enough? guess x)
+(< (abs (- (cube guess) x)) 0.001))
+
+(define (improve guess x)
+  (/ (+ (/ x guess guess)
+        (* 2 guess))
+     3))
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-root-iter (improve guess x)
+                 x)))
+
+(cube-root-iter 1.00 8)
