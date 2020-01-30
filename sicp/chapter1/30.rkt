69fd13ec722dbd5e83de8c2ad2c953a264dd7fdf
Aleksandr Maniunin <aamanunin@gmail.com>
2020-01-31 00:02:23 +0700

    complete 1_30

---
+++ sicp/chapter1/30.rkt
@@ -0,0 +1,34 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (yk k) (f (+ a (* k h))))
+  (define (inc x) (+ x 1))
+  (define (term k)
+    (* (cond ((= k 0) 1)
+             ((= n k) 1)
+             ((even? k) 2)
+             (else 4))
+       (yk k)))
+  (* (/ h 3) (sum-iter term 0 inc n)))
+
+(simpson cube 0 1 100)
+
+(simpson cube 0 1 1000)
